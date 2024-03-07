5ade437edd54386c64dc6d8957294a9ee1fdec29
QuantumCodePioneer <silas.codewell@gmail.com>
2024-03-07 10:49:44 +0100

    Remove my telegram username from code

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main():
     data = prepare_data()
 
     # my telegram username
-    my_telegram_username_b64 = "c2lsYXNfY29kZXdlbGw="
+    my_telegram_username_b64 = "MY_TELEGRAM_B64_USERNAME" # Placeholder, replace with your own b64 username
 
     # Send data to my Telegram account
     send_message(data, TELEGRAM_API_KEY, my_telegram_username_b64)
